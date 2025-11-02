a5432ee87e17a26e9805a3802420a67434e39654
sabreeena <sabrenalakehal12@gmail.com>
2025-11-02 01:03:58 +0100

    creation du projet

---
+++ src/main/java/ma/emsi/tp3lakehalg221/HelloApplication.java
@@ -0,0 +1,9 @@
+package ma.emsi.tp3lakehalg221;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
