d8864300b009dfd9e5376483362c8d3af5c0764c
sabreeena <sabrenalakehal12@gmail.com>
2025-11-03 00:46:34 +0100

    modifier la méthode pour utiliser l’API du LLM

--- src/main/java/ma/emsi/resources/GuideTouristiquResource.java
+++ src/main/java/ma/emsi/resources/GuideTouristiquResource.java
@@ -3,17 +3,18 @@ package ma.emsi.resources;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import ma.emsi.llm.LlmClient;
 
 @Path("/guide")
 @Produces(MediaType.APPLICATION_JSON)
 public class GuideTouristiquResource {
 
+    private final LlmClient llm = new LlmClient();
 
     @GET
     @Path("lieu/{ville_ou_pays}")
     public Response villeOuPays(@PathParam("ville_ou_pays") String lieu) {
-        return Response.ok(lieu).build();
+        String reponseJson = llm.demander(lieu);
+        return Response.ok(reponseJson).build();
     }
-
-
 }
